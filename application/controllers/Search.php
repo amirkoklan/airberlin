e68c0e92f5abfda022e7f3b943a0d28420655b90
Amir Koklan <amirkoklan@gmail.com>
2017-01-16 17:10:25 +0100

    commit

--- application/controllers/Search.php
+++ application/controllers/Search.php
@@ -9,12 +9,10 @@ class Search extends CI_Controller {
             
             if($this->input->get()){
                 $searchResutls = $this->database_model->getResultsByDestinationAndDeparture($this->input->get()['departure'],$this->input->get()['destination']);
-                //$results = $this->database_model->get('mileage_credit',$id);
             }
             
             $departures = $this->database_model->getDepartures();
-            $destinations = $this->database_model->getDestinations();
-            
+            $destinations = $this->database_model->getDestinations();            
             
             $data = array('destinations' => $destinations,'departures'=>$departures, 'searchResults' => $searchResutls);
             $this->load->view('search',$data);
